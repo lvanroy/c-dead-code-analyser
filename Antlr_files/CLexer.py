195b89d1c744a42ac0a33a2b769d61071dfc4251
larsVanRoy <larsvanroy@hotmail.com>
2020-03-02 18:56:23 +0100

    updated to antlr 4.8

--- Antlr_files/CLexer.py
+++ Antlr_files/CLexer.py
@@ -1,4 +1,4 @@
-# Generated from ./C.g4 by ANTLR 4.7.2
+# Generated from C.g4 by ANTLR 4.8
 from antlr4 import *
 from io import StringIO
 from typing.io import TextIO
@@ -841,7 +841,7 @@ class CLexer(Lexer):
 
     def __init__(self, input=None, output:TextIO = sys.stdout):
         super().__init__(input, output)
-        self.checkVersion("4.7.2")
+        self.checkVersion("4.8")
         self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
         self._actions = None
         self._predicates = None
